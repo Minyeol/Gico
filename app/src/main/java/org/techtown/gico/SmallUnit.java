93a5ebef0465a5f39b3c789362a5ca9b24bead7f
armyants531 <giantmathbox@gmail.com>
2021-08-22 22:28:47 +0900

    second_commit

---
+++ app/src/main/java/org/techtown/gico/SmallUnit.java
@@ -0,0 +1,27 @@
+package org.techtown.gico;
+
+public class SmallUnit {
+    String smallUnitNumber;
+    String smallUnitName;
+
+    public SmallUnit(String smallUnitNumber, String smallUnitName) {
+        this.smallUnitNumber = smallUnitNumber;
+        this.smallUnitName = smallUnitName;
+    }
+
+    public String getSmallUnitNumber() {
+        return smallUnitNumber;
+    }
+
+    public void setSmallUnitNumber(String smallUnitNumber) {
+        this.smallUnitNumber = smallUnitNumber;
+    }
+
+    public String getSmallUnitName() {
+        return smallUnitName;
+    }
+
+    public void setSmallUnitName(String smallUnitName) {
+        this.smallUnitName = smallUnitName;
+    }
+}
