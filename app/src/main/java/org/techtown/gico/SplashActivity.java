85a8033c777fb39e7ba5e4039a6919659854f28b
Binnie <binnie723@naver.com>
2021-09-26 15:18:33 +0900

    Splash Screen-오류수정3

---
+++ app/src/main/java/org/techtown/gico/SplashActivity.java
@@ -0,0 +1,36 @@
+package org.techtown.gico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        /* 특정 시간동안 Splash 화면을 띄우는 Handler 구현 */
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+    }
+
+    protected void onPause(){
+
+        super.onPause();
+        finish();
+    }
+
+}
