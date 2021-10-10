00f62295e6b6d9b12f491f2a1a6a2e97b00df365
armyants531 <giantmathbox@gmail.com>
2021-10-10 14:31:46 +0900

    8, 10 준비2

--- app/src/main/java/org/techtown/gico/study/unit10/FragmentContent10_1.java
+++ app/src/main/java/org/techtown/gico/study/unit10/FragmentContent10_1.java
@@ -3,6 +3,7 @@ package org.techtown.gico.study.unit10;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,8 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 import org.techtown.gico.R;
+import org.techtown.gico.StudyFragment;
+import org.techtown.gico.study.unit8.FragmentContent8_2;
 
 public class FragmentContent10_1 extends Fragment {
 
@@ -17,7 +20,34 @@ public class FragmentContent10_1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_content10_1, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_content10_1, container, false);
+
+        homeButton10_1 = rootView.findViewById(R.id.homeButton10_1);
+        forwardButton10_1 = rootView.findViewById(R.id.forwardButton10_1);
+
+        homeButton10_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 홈 화면으로 전환
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                StudyFragment studyFragment = new StudyFragment();
+                transaction.replace(R.id.container, studyFragment);
+                transaction.commit();
+            }
+        });
+
+        forwardButton10_1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // 다음 소단원
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                FragmentContent10_2 fragmentContent10_2 = new FragmentContent10_2();
+                transaction.replace(R.id.container, fragmentContent10_2);
+                transaction.commit();
+            }
+        });
+
+        return rootView;
     }
 }
--- app/src/main/java/org/techtown/gico/study/unit10/FragmentContent10_4.java
+++ app/src/main/java/org/techtown/gico/study/unit10/FragmentContent10_4.java
@@ -3,64 +3,52 @@ package org.techtown.gico.study.unit10;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import org.techtown.gico.R;
+import org.techtown.gico.StudyFragment;
+import org.techtown.gico.study.unit8.FragmentContent8_2;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentContent10_4#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentContent10_4 extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FragmentContent10_4() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentContent10_4.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentContent10_4 newInstance(String param1, String param2) {
-        FragmentContent10_4 fragment = new FragmentContent10_4();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    ImageButton backButton10_4, homeButton10_4, forwardButton10_4;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_content10_4, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_content10_4, container, false);
+
+        backButton10_4 = rootView.findViewById(R.id.backButton10_4);
+        homeButton10_4 = rootView.findViewById(R.id.homeButton10_4);
+        forwardButton10_4 = rootView.findViewById(R.id.forwardButton10_4);
+
+        backButton10_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 이전 화면으로 전환
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                FragmentContent10_2 fragmentContent10_2 = new FragmentContent10_2();
+                transaction.replace(R.id.container, fragmentContent10_2);
+                transaction.commit();
+            }
+        });
+
+        homeButton10_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 홈 화면으로 전환
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                StudyFragment studyFragment = new StudyFragment();
+                transaction.replace(R.id.container, studyFragment);
+                transaction.commit();
+            }
+        });
+
+        return rootView;
     }
 }
