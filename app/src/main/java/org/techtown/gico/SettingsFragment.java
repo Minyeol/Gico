bb993f92c8a5866e34edca1bc276a4b45c742b3b
Minyeol <willy3690@gm.gist.ac.kr>
2021-11-14 00:23:31 +0900

    텍스트 크기 조절 시작

--- app/src/main/java/org/techtown/gico/SettingsFragment.java
+++ app/src/main/java/org/techtown/gico/SettingsFragment.java
@@ -4,16 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.Dimension;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.w3c.dom.Text;
+
 public class SettingsFragment extends Fragment {
+    public static int TEXT_SIZE = 14;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_settings, container, false);
-
+        Button button = rootView.findViewById(R.id.button_setting);
+        TextView textView = rootView.findViewById(R.id.textView_setting);
+        textView.setTextSize(Dimension.SP, TEXT_SIZE);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TEXT_SIZE += 2;
+                if(TEXT_SIZE>20)
+                    TEXT_SIZE = 14;
+                textView.setTextSize(Dimension.SP, TEXT_SIZE);
+            }
+        });
 
         return rootView;
     }
--- app/src/main/java/org/techtown/gico/problems/unit12/FragmentProblemsContent12_1.java
+++ app/src/main/java/org/techtown/gico/problems/unit12/FragmentProblemsContent12_1.java
@@ -5,13 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.annotation.Dimension;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import org.techtown.gico.ProblemsFragment;
 import org.techtown.gico.R;
+import org.techtown.gico.SettingsFragment;
 import org.techtown.gico.problems.unit6.FragmentProblemsContent6_2;
+
+import java.util.Set;
 
 public class FragmentProblemsContent12_1 extends Fragment {
 
@@ -22,7 +27,8 @@ public class FragmentProblemsContent12_1 extends Fragment {
                              Bundle savedInstanceState) {
 
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_content12_1_problems, container, false);
-
+        TextView textView = rootView.findViewById(R.id.textView121212);
+        textView.setTextSize(Dimension.SP, SettingsFragment.TEXT_SIZE);
         homeButton12_1 = rootView.findViewById(R.id.homeButton12_1);
         //forwardButton12_1 = rootView.findViewById(R.id.forwardButton12_1);
 
